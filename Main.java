1757fc10afe1f6a5e3b151dfa9b784891e4501bf
MIKULIC Teo <o22101637@info21-21.iut45.univ-orleans.fr>
2023-03-15 18:01:09 +0100

    Debut du projet

---
+++ Main.java
@@ -0,0 +1,9 @@
+package puissance4;
+
+import puissance4.views.MenuView;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuView.showMenu(); // Affiche le menu principal du jeu
+    }
+}
